19fc4e611ade47b02e2561221852692212c53017
Juan Martinez <juan@orbelink.com>
2016-05-31 21:01:23 -0600

    final

--- resource/scripts.js
+++ resource/scripts.js
@@ -1,8 +1,25 @@
-window.onload = function(e){
-    var dataObject = JSON.parse(simularDatos());
-    console.log(dataObject);
-}
+(function($){
+	$(document).ready(function(){
+		var dataObject = JSON.parse(simularDatos());
+		for (var i = dataObject.data.length - 1; i >= 0; i--) {
+			var typeCircle = dataObject.data[i].Circle;
+			var edad = dataObject.data[i].Edad;
+			var nombre = dataObject.data[i].Nombre;
+			var genero = dataObject.data[i].Genero;
+
+			var color = 'red';
+			if(genero == 'Masculino'){
+				color = 'blue';
+			}
+
+			$('circle#'+typeCircle).attr("r", edad);
+			$('circle#'+typeCircle).attr("fill", color);
+			$('text#'+typeCircle+'-label').text(nombre);
+		}		
+
+	});
+})(jQuery);
 
 function simularDatos(){
-    return '{"data":[{"Nombre":"Carlos","Edad":50,"Genero":"Masculino"},{"Nombre":"Jose","Edad":23,"Genero":"Masculino"},{"Nombre":"María","Edad":45,"Genero":"Femenino"}]}';
+    return '{"data":[{"Circle":"first","Nombre":"Carlos","Edad":50,"Genero":"Masculino"},{"Circle":"second","Nombre":"Jose","Edad":23,"Genero":"Masculino"},{"Circle":"third","Nombre":"María","Edad":45,"Genero":"Femenino"}]}';
 }
