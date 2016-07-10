984f2c8c3596df646461586a6ff66ff2085da048
Juan <juanmnl07@gmail.com>
2016-07-10 13:34:44 -0600

    eliminar archivo de asociaciones

--- variables.js
+++ variables.js
@@ -7,21 +7,7 @@
 	var label = '<text dx="40" dy="55">';
 
 $(document).ready(function(){
-	//Inicializar las variables globales
-	function set_data(data_circle){
-		$.each(data_circle, function(k, v){
-			//incluir atributos
-			circle += k + '="' + v + '"';
-			if(k == 'label'){
-				label += v + '</text>';//label
-			}
-		});
-		circle += ' />'; 
-	}
-	//Devolver el grafico
-	function get_data(){
-		return circle + label;
-	}
+	
 });
 
 })(jQuery);
