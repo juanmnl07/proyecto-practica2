17eaca123056b3498af706d6b634b4dee4d601a4
Juan Martinez <juan@orbelink.com>
2016-07-24 13:18:17 -0600

    ajustes

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,10 +4,6 @@
 	var asociaciones = {"asociaciones":[{}]};
 	var globalData = [];
 
-	//etiquieta svg
-	var svg1 = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-	svg1.setAttribute("height",200);
-	svg1.setAttribute("width",1000);
 	//****** END GLOBALS ******
 
 	$(document).ready(function(){
@@ -55,7 +51,7 @@
 					//incluir el archivo que se encarga de dibujar circulos
 					//recorrer la estructura de asociaciones para imprimirlos en pantalla
 					//remover graficos
-					removeCircles();
+					limpiarCanvas();
 					printGraphAndAssociations();
 				}
 			}
@@ -119,56 +115,29 @@
 			});
 		}
 
-		function removeCircles(){
-			$('#graficos svg').remove();
-		}
+		
 
 		function printGraphAndAssociations(){
-			$('#graficos').append(svg1);
 			var total_x = 0;
 			for (var i = 0; i < globalData.length; i++) {
-				var arrayObjeto = [];
 				jsonObject = JSON.parse(globalData[i], function(k, v){
 					$.each(asociaciones, function(k2, v2){
 						$.each(v2, function(k3, v3){
 							$.each(v3, function(k4, v4){
-								var values = {};
 								if(k == k4){
 									var atr = v4.replace("atr_","");
-									values[atr] = v;
 									if(atr == 'r'){
 										total_x += v;
-										values['cx'] = (total_x * 2) + 10;
+										setAtr_cx((total_x * 2) + 10);
+										setAtr_r(v);
 									}
-									arrayObjeto.push(values);
 								}
 							});
 						});
 					})
 				});
-				set_data(arrayObjeto);
+				draw();
 			}
-		}
-
-		//Inicializar las variables globales
-		function set_data(data_circle){
-			var circles = document.createElementNS("http://www.w3.org/2000/svg", "circle");
-			for (var i = 0; i < data_circle.length; i++) {
-				$.each(data_circle[i], function(k, v){
-					circles.setAttribute(k,v);
-					if(k == 'fill'){
-						var color = '#F5A9F2';
-						if(v == 'Hombre'){
-							color = 'blue';
-						}
-						circles.setAttribute(k,color);
-					}
-					circles.setAttribute("cy",100); 
-					circles.setAttribute('stroke-width',3);
-
-				});
-			};
-			svg1.appendChild(circles);
 		}
 	})
 })(jQuery);
--- variables.js
+++ variables.js
@@ -1,13 +1,62 @@
-(function($){
-	var atr_r = '';
-	var atr_fill = '';
-	var atr_label = '';
-	var atr_stroke = '';
-	var circle = '<circle ';
-	var label = '<text dx="40" dy="55">';
+//etiquieta svg
+var svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+svg.setAttribute("height",200);
+svg.setAttribute("width",1000);
 
-$(document).ready(function(){
-	
-});
+//variables atributos
+var atr_r = '';
+var atr_fill = '';
+var atr_label = '';
+var atr_stroke = '';
 
-})(jQuery);
+//funciones set para cada atributo
+function setAtr_r(val){
+	atr_r = val;
+}
+
+function setAtr_fill(val){
+	atr_fill = val;
+}
+
+function setAtr_label(val){
+	atr_label = val;
+}
+
+function setAtr_stroke(val){
+	atr_stroke = val;
+}
+
+function setAtr_cx(val){
+	atr_cx = val;
+}
+
+function limpiarCanvas(){
+	jQuery('#graficos svg').remove();
+	jQuery('#graficos').append(svg);
+}
+
+function draw(){
+	var circle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+	circle.setAttribute("cy",100); 
+	circle.setAttribute("cx", atr_cx);
+	circle.setAttribute('stroke-width',3);
+	circle.setAttribute("color", atr_fill);
+	circle.setAttribute("stroke", atr_stroke);
+	circle.setAttribute("r", atr_r);
+	svg.appendChild(circle);
+}
+
+/*function set_data(data_circle){
+	for (var i = 0; i < data_circle.length; i++) {
+		$.each(data_circle[i], function(k, v){
+			circles.setAttribute(k,v);
+			if(k == 'fill'){
+				var color = '#F5A9F2';
+				if(v == 'Hombre'){
+					color = 'blue';
+				}
+				circles.setAttribute(k,color);
+			}		
+		});
+	};
+}*/
